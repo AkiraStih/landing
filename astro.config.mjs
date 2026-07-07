a027639fef38ffc01e8a6cf3287b520b8b778173
Bogdan <your_email@example.com>
2026-07-07 20:46:24 +0300

    wip: make base style, add fonts and add svg icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Montserrat',
+      cssVariable: '--font-montserrat',
+      weights: [400, 600, 700],
+    },
+     {
+      provider: fontProviders.google(),
+      name: 'Yuyu',
+      cssVariable: '--font-yuyu',
+      weights: [400, 600, 700],
+    },
+  ],
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
